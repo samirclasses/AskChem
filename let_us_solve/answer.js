3adfe346e8508f585c66fe69037d0fffb822a9b8
Samir Dutta <132768133+samirclasses@users.noreply.github.com>
2025-08-19 21:32:47 +0530

    Update answer.js

--- let_us_solve/answer.js
+++ let_us_solve/answer.js
@@ -1,3 +1,7 @@
+function ans57()
+{alert('Correct Answer is  B')
+    
+}
 function ans56()
 {alert('Correct Answer is  D')
     
@@ -219,6 +223,7 @@ function ans9()
 function ans10()
 {alert('Correct Answer is  D')
 }
+
 
 
 
